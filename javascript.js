b062e7697b6e31dc3bd59f638540e3bd8036eaa3
James Hunt <37090809+James-J-Hunt@users.noreply.github.com>
2019-04-24 17:52:40 +1200

    Add files via upload
    Alpha Beta and Gamma working on Laptop, to test on phone

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
 if(window.DeviceOrientationEvent) { 
-    document.write ("YEET");
+    document.write ("1");
 }
+window.addEventListener('deviceorientation', function(event) {
+    var alpha = event.alpha;
+    var beta = event.beta;
+    var gamma = event.gamma;
+    document.write (alpha);
+    document.write ('<p>');
+    document.write (beta);
+    document.write ('<p>');
+    document.write (gamma);
+}, false);
