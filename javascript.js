122967ce6300f27a420c5de7652c857352dcbf7b
James Hunt <37090809+James-J-Hunt@users.noreply.github.com>
2019-04-25 17:02:06 +1200

    Intergration between the two scripts

--- javascript.js
+++ javascript.js
@@ -1,29 +1,41 @@
 function init() {
-        var compass = document.getElementById('compass');
-        if(window.DeviceOrientationEvent) {
+    //Find our div containers in the HTML
+    var dataContainerOrientation = document.getElementById('dataContainerOrientation');
+    var compass = document.getElementById('compass');
+    
+    if(window.DeviceOrientationEvent) {
   
-          window.addEventListener('deviceorientation', function(event) {
-                var alpha;
-                //Check for iOS property
-                if(event.webkitCompassHeading) {
-                  alpha = event.webkitCompassHeading;
-                  //Rotation is reversed for iOS
-                  compass.style.WebkitTransform = 'rotate(-' + alpha + 'deg)';
-                }
-                //non iOS
-                else {
-                  alpha = event.alpha;
-                  webkitAlpha = alpha;
-                  if(!window.chrome) {
+        window.addEventListener('deviceorientation', function(event) {
+            //Variable for the compass position
+            var alpha;
+
+            //Variables for the rest of the iphone position
+            var beta = event.beta;
+            var gamma = event.gamma;
+
+            //Check for iOS property
+            if(event.webkitCompassHeading) {
+                alpha = event.webkitCompassHeading;
+                //Rotation is reversed for iOS
+                compass.style.WebkitTransform = 'rotate(-' + alpha + 'deg)';
+            }
+            //non iOS
+            else {
+                alpha = event.alpha;
+                webkitAlpha = alpha;
+                if(!window.chrome) {
                     //Assume Android stock (this is crude, but good enough for our example) and apply offset
                     webkitAlpha = alpha-270;
-                  }
                 }
+            }
  
-                compass.style.Transform = 'rotate(' + alpha + 'deg)';
-                compass.style.WebkitTransform = 'rotate('+ webkitAlpha + 'deg)';
-                //Rotation is reversed for FF
-                compass.style.MozTransform = 'rotate(-' + alpha + 'deg)'; 
-              }, false);
-        }
-      }
+            compass.style.Transform = 'rotate(' + alpha + 'deg)';
+            compass.style.WebkitTransform = 'rotate('+ webkitAlpha + 'deg)';
+            //Rotation is reversed for FF
+            compass.style.MozTransform = 'rotate(-' + alpha + 'deg)'; 
+
+            if(alpha!=null || beta!=null || gamma!=null) 
+                dataContainerOrientation.innerHTML = 'alpha: ' + alpha + '<br/>beta: ' + beta + '<br />gamma: ' + gamma;
+        }, false);
+    }
+}
