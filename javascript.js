c3509ad5b8c7e4483bbe67c7c61a9f49b7b4224e
James Hunt <37090809+James-J-Hunt@users.noreply.github.com>
2019-04-25 16:21:27 +1200

    Compass Test 234

--- javascript.js
+++ javascript.js
@@ -1,31 +1,29 @@
 function init() {
-    //Find our div containers in the DOM
-    var dataContainerOrientation = document.getElementById('dataContainerOrientation');
-
-    //Check for support for DeviceOrientation event     
-    if(window.DeviceOrientationEvent) {
-        window.addEventListener('deviceorientation', function(event) {
-
-            //Sets the alpha variable
-            var alpha;
-            //Check for iOS property
-            if(event.webkitCompassHeading) {
-                alpha = event.webkitCompassHeading;
-                //Rotation is reversed for iOS
-                compass.style.WebkitTransform = 'rotate(-' + alpha + 'deg)';
-            }
-            //non iOS
-            else {
-                alpha = event.alpha;
-                webkitAlpha = alpha;
-                if(!window.chrome) {
+        var compass = document.getElementById('compass');
+        if(window.DeviceOrientationEvent) {
+  
+          window.addEventListener('deviceorientation', function(event) {
+                var alpha;
+                //Check for iOS property
+                if(event.webkitCompassHeading) {
+                  alpha = event.webkitCompassHeading;
+                  //Rotation is reversed for iOS
+                  compass.style.WebkitTransform = 'rotate(-' + alpha + 'deg)';
+                }
+                //non iOS
+                else {
+                  alpha = event.alpha;
+                  webkitAlpha = alpha;
+                  if(!window.chrome) {
                     //Assume Android stock (this is crude, but good enough for our example) and apply offset
                     webkitAlpha = alpha-270;
+                  }
                 }
-            }
-
-            if(alpha!=null) 
-                dataContainerOrientation.innerHTML = 'alpha: ' + alpha;
-        }, false);
-    }
-}
+ 
+                compass.style.Transform = 'rotate(' + alpha + 'deg)';
+                compass.style.WebkitTransform = 'rotate('+ webkitAlpha + 'deg)';
+                //Rotation is reversed for FF
+                compass.style.MozTransform = 'rotate(-' + alpha + 'deg)'; 
+              }, false);
+        }
+      }
